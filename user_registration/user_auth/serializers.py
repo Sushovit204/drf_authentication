1065bed60ec42dcc2c8c3521d813dbbe277dc0f6
Sushovit204 <shahsushovit434@gmail.com>
2024-03-17 18:18:16 +0545

    Added the hash logic and set password to write only: True in serializer meta

--- user_registration/user_auth/serializers.py
+++ user_registration/user_auth/serializers.py
@@ -6,3 +6,15 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'name', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        """This create method is called by DRF when creating a new object which later hashs the password"""
+        password = validated_data.pop("password", None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
