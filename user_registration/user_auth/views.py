e731cb9d8823a217cb55699312fb34a9b645540e
Sushovit204 <shahsushovit434@gmail.com>
2024-03-17 22:23:16 +0545

    Added the Login View

--- user_registration/user_auth/views.py
+++ user_registration/user_auth/views.py
@@ -1,6 +1,9 @@
 from rest_framework.views import APIView
 from .serializers import UserSerializer
 from rest_framework.response import Response
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.exceptions import AuthenticationFailed
+from .models import User
 
 class RegisterView(APIView):
     """View for registering the user"""
@@ -9,3 +12,22 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    
+
+class LoginView(APIView):
+    """View for loging th user"""
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        try:
+            user = User.objects.get(email=email)
+        except ObjectDoesNotExist:
+            raise AuthenticationFailed('User Not Found')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect Password')
+        
+        return Response({
+            'message':'Login Successful'
+        })
